a01be168b9069905e90491f9b36b0f57c3c3dd60
Manvi Baunthiyal <120104921+manvibaunthiyal@users.noreply.github.com>
2024-03-05 22:16:52 +0530

    Disease_pridiction.py

--- Disease_pridiction.py
+++ Disease_pridiction.py
@@ -109,9 +109,9 @@ def NaiveBayes():
         t3.delete("1.0", END)
         t3.insert(END, "No Disease")
 
-rohitGUI = Tk()
-rohitGUI.title(" Disease Prediction Based on Symptoms")
-rohitGUI.configure()
+manviGUI = Tk()
+manviGUI.title(" Disease Prediction Based on Symptoms")
+manviGUI.configure()
 
 Symptom1 = StringVar()
 Symptom1.set(None)
@@ -127,65 +127,65 @@ Symptom5.set(None)
 
 #GUI part
 
-w2 = Label(rohitGUI, justify=LEFT, text=" Disease Prediction Based on Symptoms ", bg="red", fg="yellow")
+w2 = Label(manviGUI, justify=LEFT, text=" Disease Prediction Based on Symptoms ", bg="red", fg="yellow")
 w2.config(font=("Arial Bold", 32))
 w2.grid(row=1, column=1, columnspan=2, padx=100)
 
-NameLb1 = Label(rohitGUI, text="")
+NameLb1 = Label(manviGUI, text="")
 NameLb1.config(font=("Elephant", 20))
 NameLb1.grid(row=5, column=1, pady=10, sticky=W)
 
-S1 = Label(rohitGUI, text="Symptom 1", fg="green")
+S1 = Label(manviGUI, text="Symptom 1", fg="green")
 S1.config(font=("Elephant", 15))
 S1.grid(row=7, column=1, pady=10 , sticky=W)
 
-S2 = Label(rohitGUI, text="Symptom 2", fg="green")
+S2 = Label(manviGUI, text="Symptom 2", fg="green")
 S2.config(font=("Elephant", 15))
 S2.grid(row=8, column=1, pady=10, sticky=W)
 
-S3 = Label(rohitGUI, text="Symptom 3", fg="green")
+S3 = Label(manviGUI, text="Symptom 3", fg="green")
 S3.config(font=("Elephant", 15))
 S3.grid(row=9, column=1, pady=10, sticky=W)
 
-S4 = Label(rohitGUI, text="Symptom 4", fg="green")
+S4 = Label(manviGUI, text="Symptom 4", fg="green")
 S4.config(font=("Elephant", 15))
 S4.grid(row=10, column=1, pady=10, sticky=W)
 
-S5 = Label(rohitGUI, text="Symptom 5", fg="green")
+S5 = Label(manviGUI, text="Symptom 5", fg="green")
 S5.config(font=("Elephant", 15))
 S5.grid(row=11, column=1, pady=10, sticky=W)
 
-lr = Button(rohitGUI, text="TAP TO PREDICT DISEASE", height=2, width=30, command=message)
+lr = Button(manviGUI, text="TAP TO PREDICT DISEASE", height=2, width=30, command=message)
 lr.config(font=("Elephant", 15),bg="yellow")
 lr.grid(row=15, column=1,pady=20)
 
 OPTIONS = sorted(l1) # puts options in list 1 in sorted order
 
-S1En = OptionMenu(rohitGUI, Symptom1, *OPTIONS)
+S1En = OptionMenu(manviGUI, Symptom1, *OPTIONS)
 S1En.grid(row=7, column=2)
 
-S2En = OptionMenu(rohitGUI, Symptom2, *OPTIONS)
+S2En = OptionMenu(manviGUI, Symptom2, *OPTIONS)
 S2En.grid(row=8, column=2)
 
-S3En = OptionMenu(rohitGUI, Symptom3, *OPTIONS)
+S3En = OptionMenu(manviGUI, Symptom3, *OPTIONS)
 S3En.grid(row=9, column=2)
 
-S4En = OptionMenu(rohitGUI, Symptom4, *OPTIONS)
+S4En = OptionMenu(manviGUI, Symptom4, *OPTIONS)
 S4En.grid(row=10, column=2)
 
-S5En = OptionMenu(rohitGUI, Symptom5, *OPTIONS)
+S5En = OptionMenu(manviGUI, Symptom5, *OPTIONS)
 S5En.grid(row=11, column=2)
 
-NameLb = Label(rohitGUI, text="")
+NameLb = Label(manviGUI, text="")
 NameLb.config(font=("Elephant", 20))
 NameLb.grid(row=13, column=1, pady=10, sticky=W)
 
-NameLb = Label(rohitGUI, text="")
+NameLb = Label(manviGUI, text="")
 NameLb.config(font=("Elephant", 15))
 NameLb.grid(row=18, column=1, pady=10, sticky=W)
 
-t3 = Text(rohitGUI, height=2, width=30, bg="grey")
+t3 = Text(manviGUI, height=2, width=30, bg="grey")
 t3.config(font=("Elephant", 20))
 t3.grid(row=20, column=1 , padx=10)
 
-rohitGUI.mainloop()
+manviGUI.mainloop()
